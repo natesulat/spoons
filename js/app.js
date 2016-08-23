13a749bbb82ff7e5df56d1df63e263c79b674f3b
natesulat <nate.sulat@ogilvy.com>
2016-08-23 15:45:22 -0400

    got the while loop worked out

--- js/app.js
+++ js/app.js
@@ -10,36 +10,49 @@ $(function () {
             var q = j;
             q === 0 ? q = 'hearts' : q === 1 ? q = 'diamonds' : q === 2 ? q = 'clubs' : q = 'spades';
             if (t > 10) {
-                t === 11 ? t = 'jack' : t === 12 ? t = 'queen' : t === 13 ? t = 'king' : t = 'ace';
+                t === 11 ? t = 'j' : t === 12 ? t = 'q' : t === 13 ? t = 'k' : t = 'a';
             }
             var newCard = new Card(t, q);
             deck.push(newCard);
         }
     };
-    
-    deck = _.shuffle(deck);
-    
-    $('#card-container').css('height', $(window).height());
-//    deck.forEach(function(i) {
-//        var item = $('<li></li').text(i.number + ' of ' + i.suit);
-//        $('#test').append(item);
-//    });
-    
-    $('button').click(function)
-    
-    while (deck.length > 0 || $('.card').length === 4) {
-        var deal = deck.shift();
-        var template = Handlebars.compile($('#card-template').html());
-        if ($('#card-container card').length <= 4) {
-            $('#card-container').append(template(deal));
-        }
-    }
-    
-    var check = [deck, deck.length];
-    logger(check);
-    
-    $('body').removeAttr('style');
 
+    deck = _.shuffle(deck);
+
+    $('#card-container').css('height', $(window).height());
+
+    var template = Handlebars.compile($('#card-template').html());
+
+    while (deck.length > 0) {
+        if ($('.card').length < 4) {
+            var template = Handlebars.compile($('#card-template').html());
+            if ($('#card-container card').length <= 4) {
+                $('#card-container').append(template(deck.shift()));
+            }
+        }
+        deck.shift();
+        console.log(deck[0]);
+    }
+
+    $('#card-container').on('click', '.card', function (e) {
+        e.preventDefault();
+        $(this).remove();
+        $('#card-container').append(template(deck.shift()));
+    });
+
+    var check = [deck[0], deck, deck.length];
+
+    logger(check);
+
+    while (deck.length > 0) {
+        deck.shift();
+        console.log(deck[0]);
+    }
+    //    while (deck.length > 0) {
+    //        logger(check);
+    //    }
+
+    $('body').removeAttr('style');
 
     function logger(arr) {
         arr.forEach(function (i) {
